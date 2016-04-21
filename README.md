9c7870fc9f9bf39c2557f100af21e129a5cd8ec1
mr snord <csnord@rvr.net>
2016-04-21 16:16:12 +0200

    Ch2 - Getting User Input

--- README.md
+++ README.md
@@ -19,4 +19,12 @@ Created two files
 * controller has
 * has scope, var=posts, var=test, and function=addPost
 
+###2 - Getting User Input
+Added a link field, as well as reflecting user input in the list.
+####File: index.html
+* added ngClick to increment votes
+* added a new field "link"
 
+####File: app.js
+* Added function on controller: incrementUpvotes
+* Added capability to add a new title, as well as a link
--- app.js
+++ app.js
@@ -10,4 +10,16 @@ function($scope){
     {title: 'post 4', upvotes: 9},
     {title: 'post 5', upvotes: 4}
   ];
+  $scope.incrementUpvotes = function(post) { post.upvotes += 1; };
+  $scope.addPost = function(){
+    // $scope.posts.push({title: 'A new post!', upvotes: 0});
+    if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
+    $scope.title = '';
+    $scope.link = '';
+  };
 }]);
